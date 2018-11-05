06b4ac079454738c03d89152f49e339da160c560
til <til@scalepoint.com>
2018-11-05 13:15:21 +0100

    Added readme and POC fetch call from JS

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Autobot
+
+## run
+
+build in visual studio or with 'dotnet watch run'
+
+navigate to /index.html
---
+++ src/autobot/wwwroot/js/fetchers.js
@@ -0,0 +1,6 @@
+﻿const init = async () => {
+    const response = await fetch('/api/values');
+    const myJson = await response.json();
+    var number = document.getElementById("init").innerHTML = myJson;
+};
+init();
