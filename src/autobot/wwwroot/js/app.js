3ba6f260455f7537677f1610e82a1d1631d2e076
til <til@scalepoint.com>
2018-11-07 21:32:06 +0100

    Make em shake

--- src/autobot/wwwroot/js/app.js
+++ src/autobot/wwwroot/js/app.js
@@ -3,3 +3,8 @@
     imgElm.classList.add("front");
     imgElm.classList.add("left");
 }
+
+function shake() {
+    var imgElm = document.getElementById("test2");
+    imgElm.classList.add("shake");
+}
