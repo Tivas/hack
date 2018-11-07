901ec01443e52953b6c717b3cb316c128235348e
til <til@scalepoint.com>
2018-11-07 21:59:48 +0100

    more lir

--- src/autobot/wwwroot/js/app.js
+++ src/autobot/wwwroot/js/app.js
@@ -1,10 +1,17 @@
 ﻿function moveFrontLeft() {
-    var imgElm = document.getElementById("test1");
+    var imgElm = document.getElementById("test2");
+    imgElm.classList.remove("shake");
     imgElm.classList.add("front");
     imgElm.classList.add("left");
 }
 
 function shake() {
     var imgElm = document.getElementById("test2");
-    imgElm.classList.add("shake");
+    if (imgElm.classList.contains("shake")) {
+        imgElm.classList.remove("shake");
+    }
+    else {
+        imgElm.classList.add("shake");
+    }
+
 }
