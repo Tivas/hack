2b4b1067f3be92d6a1e6ff7cc9883bad58baa168
Alexander Chernenko <chea@scalepoint.com>
2018-11-07 15:00:26 +0100

    sample image

--- src/autobot/Controllers/ForsiController.cs
+++ src/autobot/Controllers/ForsiController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.IO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace autobot.Controllers
@@ -11,5 +7,27 @@ namespace autobot.Controllers
     [ApiController]
     public class ForsiController : ControllerBase
     {
+	    [HttpGet("getReportImages/{proformaNo}")]
+	    public ActionResult<string> GetReportImages(string proformaNo)
+	    {
+		    var img = new byte[] { };
+		    if (proformaNo == "RP12345")
+		    {
+			    img = GetStaticImage();
+		    }
+		    return new JsonResult(img); // File(img, "image/jpeg");
+	    }
+
+	    private static byte[] GetStaticImage()
+	    {
+		    var a = System.Reflection.Assembly.GetExecutingAssembly();
+		    using (var resFilestream = a.GetManifestResourceStream("autobot.Resources.mazda.jpg"))
+		    {
+			    if (resFilestream == null) return null;
+			    var ba = new byte[resFilestream.Length];
+			    resFilestream.Read(ba, 0, ba.Length);
+			    return ba;
+		    }
+	    }
     }
 }
